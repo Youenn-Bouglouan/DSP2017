79b373de1c5fb451746f8ac581e9a81478a292b9
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2017-04-24 20:24:01 +0200

    added new benchmark projects for both F# and C#

---
+++ HelloWebSharper/BenchmarkDotNetCsharp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkDotNetCsharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PerformanceTests>();
+            Console.WriteLine(summary);
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class PerformanceTests
+    {
+        private static string[] _names = 
+        {
+            "John", "Albert", "Tomasz", "Jean-Pierre", "Nathalie",
+            "Samantha", "Edward", "Tanya", "Michelle", "Kurt",
+            "Justine", "Christian", "Christopher", "Andres", "Katarzyna",
+            "Leon", "Adrian", "Sheldon", "Maxime", "Monika"
+        };
+
+        private static bool[] _booleans = { true, false };
+
+        private List<Customer> _customers;
+
+        [Setup]
+        public void CSharp_GenerateCustomers()
+        {
+            var numberToGenerate = 1000000;
+            var random = new Random(10);
+            var customers = new List<Customer>();
+            
+            for (int index = 1; index <= numberToGenerate; index++)
+            {
+                var customer = new Customer()
+                {
+                    Id = index,
+                    Age = random.Next(18, 100),
+                    Name = _names[random.Next(0, _names.Length)],
+                    IsVip = _booleans[random.Next(0, _booleans.Length)],
+                    CustomerSince = DateTime.Today.AddMonths(-random.Next(0, 121))
+                };
+
+                customers.Add(customer);
+            }
+
+            _customers = customers;
+
+            // Check the number of VIP customers:
+            // CSharp_GenerateCustomers VIP customers count: 80 251 / 100 000
+            // CSharp_GenerateCustomers VIP customers count: 801 227 / 1 000 000
+            
+//            var today = DateTime.Today;
+//            var vipCustomersCount = _customers.Count(c => (today - c.CustomerSince).TotalDays > 365 * 2);
+//            Console.WriteLine($"CSharp_GenerateCustomers VIP customers count: {vipCustomersCount}");
+        }
+
+        [Benchmark]
+        public void CSharp_ModifyCustomers()
+        {
+            var today = DateTime.Today;
+
+            foreach (var customer in _customers)
+            {
+                // If customer for more than 2 years
+                if ((today - customer.CustomerSince).TotalDays > 365*2)
+                {
+                    customer.IsVip = true;
+                }
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsVip { get; set; }
+        public DateTime CustomerSince { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Age}, {IsVip}, {CustomerSince}";
+        }
+    }
+}
---
+++ HelloWebSharper/BenchmarkDotNetFsharp/Program.fs
@@ -0,0 +1,127 @@
+﻿open System
+open System.Collections.Generic
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Attributes.Jobs
+
+type Customer = {
+    Id: int
+    Name: string
+    Age: int
+    IsVip: bool
+    CustomerSince: DateTime
+}
+
+let print c =
+    printfn "%i, %s, %i, %b, %A" c.Id c.Name c.Age c.IsVip c.CustomerSince
+
+let setVip today customer =
+    match (today - customer.CustomerSince).TotalDays > 365. * 2. with
+    | true -> { customer with IsVip = true }
+    | false -> customer
+
+let names = [|
+    "John"; "Albert"; "Tomasz"; "Jean-Pierre"; "Nathalie";
+    "Samantha"; "Edward"; "Tanya"; "Michelle"; "Kurt";
+    "Justine"; "Christian"; "Christopher"; "Andres"; "Katarzyna";
+    "Leon"; "Adrian"; "Sheldon"; "Maxime"; "Monika"
+|]
+
+let booleans = [| true; false |]
+
+[<MemoryDiagnoser>]
+type PerformanceTests_ImmutableList() =
+    
+    let mutable _customers : Customer list = [] // Immutable F# linked-list
+    member this.GetCustomers() = _customers
+   
+    [<Setup>]
+    member this.FSharp_GenerateCustomers_Immutable() =
+        let numberToGenerate = 1000000
+        let random = Random(10)
+        
+        let rec loop customers max current =
+            if current > max then
+                customers
+            else
+                let customer = {
+                    Id = current
+                    Age = random.Next(18, 100)
+                    Name = names.[random.Next(0, names.Length)]
+                    IsVip = booleans.[random.Next(0, booleans.Length)]
+                    CustomerSince = DateTime.Today.AddMonths(-random.Next(0, 121))
+                }
+
+                loop (customer::customers) max (current+1)
+           
+        // Assign the results to our F# immutable list of customers
+        _customers <- loop [] numberToGenerate 1
+
+    [<Benchmark>]
+    member this.FSharp_ModifyCustomers_Immutable() =
+        let today = DateTime.Today
+
+        _customers
+        |> List.map (setVip today)
+        |> ignore
+
+// ---------------------------------------------------------------------------------------------
+
+[<MemoryDiagnoser>]
+type PerformanceTests_MutableList() =
+    
+    let mutable _customers  = List<Customer>() // Mutable standard .NET list
+    member this.GetCustomers() = _customers
+
+    [<Setup>]
+    member this.FSharp_GenerateCustomers_Mutable() =
+        let numberToGenerate = 100000
+        let random = Random(10)
+        let customers = List<Customer>()
+
+        for i = 1 to numberToGenerate do
+            let customer = {
+                    Id = i
+                    Age = random.Next(18, 100)
+                    Name = names.[random.Next(0, names.Length)]
+                    IsVip = booleans.[random.Next(0, booleans.Length)]
+                    CustomerSince = DateTime.Today.AddMonths(-random.Next(0, 121))
+            }
+            customers.Add(customer)
+           
+        // Assign the results to our C#-like mutable list of customers
+        _customers <- customers
+
+    [<Benchmark>]
+    member this.FSharp_ModifyCustomers_Mutable() =
+        let today = DateTime.Today
+    
+        let rec loop index (customers:List<Customer>) =
+            if index >= customers.Count
+            then ignore
+            else 
+                customers.[index] <- setVip today customers.[index]
+                loop (index+1) customers
+
+        loop 0 _customers |> ignore
+  
+[<EntryPoint>]
+let main argv = 
+    
+    (*    
+    printfn "start %A" DateTime.Now
+    let perfTests = PerformanceTests()
+    perfTests.FSharp_GenerateCustomers() |> ignore
+    //perfTests.GetCustomers() |> List.iter print
+    //perfTests.GetCustomers2() |> Seq.iter print
+    printfn "inter %A" DateTime.Now
+    //perfTests.FSharp_ModifyCustomers() |> ignore // |> List.iter print
+    perfTests.FSharp_ModifyCustomers2() |> ignore
+    //perfTests.GetCustomers2() |> Seq.iter print
+    printfn "end %A" DateTime.Now
+    *)
+
+    //BenchmarkRunner.Run<PerformanceTests_ImmutableList>() |> printfn "%A"
+    BenchmarkRunner.Run<PerformanceTests_MutableList>() |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0
